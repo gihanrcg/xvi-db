4fed10ec62ef19c64467eb6f76c7adf9bd418d75
gsir2992 <gihan.siriwardhana@syscolabs.com>
2022-06-19 12:44:13 +0530

    updated links

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const UserCard = ({name, al, contact, job}) => {
+export const UserCard = ({name, al, contact, job, email}) => {
   return (
     <div className='grid-item'>
         <div className='card text-center'>
@@ -9,7 +9,8 @@ export const UserCard = ({name, al, contact, job}) => {
             </h5>
             <div class="card-body">
                 <h5 className="card-title">A/L : {al}</h5>
-                <p className="card-text">Contact : {contact}</p>
+                <p className="card-text">Contact : <a href={`tel:${contact}`} class="link-secondary">{contact}</a></p>
+                <p className="card-text">Email : <a href={`mailto:${email}`} class="link-secondary">{email}</a></p>
                 <p className="card-text">Occupation : {job}</p>
             </div>
         </div>
